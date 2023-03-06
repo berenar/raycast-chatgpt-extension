1069ff4b482ba353c86ada5c9f04a45d504f7735
berenar <bernatpericasserra97@gmail.com>
2023-03-06 19:09:01 +0100

    Basic form and Draft feature

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,52 @@
-import { Detail } from "@raycast/api";
-export default function Command() {
-  return <Detail markdown="**Hello** _World_!" />;
+import { Action, ActionPanel, Detail, Form, LaunchProps, popToRoot } from "@raycast/api";
+import { useState } from "react";
+
+interface FormValues {
+  inputField: string;
+}
+
+export default function Command(props: LaunchProps<{ draftValues: FormValues }>) {
+  const { draftValues } = props;
+
+  const [nameError, setNameError] = useState<string | undefined>();
+
+  function dropNameErrorIfNeeded() {
+    if (nameError && nameError.length > 0) {
+      setNameError(undefined);
+    }
+  }
+
+  return (
+    <>
+      <Detail markdown="**ChatGPT**" />
+      <Form
+        enableDrafts
+        actions={
+          <ActionPanel>
+            <Action.SubmitForm
+              onSubmit={(values: FormValues) => {
+                console.log("onSubmit", values);
+                popToRoot();
+              }}
+            />
+          </ActionPanel>
+        }
+      >
+        <Form.TextField
+          id="inputField"
+          title="Prompt"
+          defaultValue={draftValues?.inputField ?? "What is Raycast?"}
+          error={nameError}
+          onChange={dropNameErrorIfNeeded}
+          onBlur={(event) => {
+            if (event.target.value?.length == 0) {
+              setNameError("The field should't be empty!");
+            } else {
+              dropNameErrorIfNeeded();
+            }
+          }}
+        />
+      </Form>
+    </>
+  );
 }
